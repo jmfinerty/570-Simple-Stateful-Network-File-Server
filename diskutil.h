cbc9f01d14e574b5dfc899a1f11bfd0d0a333fa4
jmfinerty <jmfinerty@gmail.com>
2021-11-16 22:37:17 -0500

    some structs to track files, their blocks, and the users associated with them

--- diskutil.h
+++ diskutil.h
@@ -1,14 +1,26 @@
 #ifndef DISKUTIL_H
 #define DISKUTIL_H
 
+// Disk
+#define VDISK_LOC      "vdisk"
+#define VDISK_CAPACITY 16777216 // 16 mB
+#define BLOCK_SIZE     512      // bytes per block
+#define FILE_SIZE      64       // blocks per file
+
+// Files
 #define DEFAULT_FILE_NAME "SOMEFILE"
-#define DEFAULT_USER_NAME "SOMEUSER"
-
-#define VDISK_LOC "vdisk"
-
-#define MAX_FT_SIZE       200
 #define MAX_FILE_NAME_LEN 20
+
+// Users
+#define DEFAULT_USER_NAME "SOMEUSER"
 #define MAX_USER_NAME_LEN 15
+#define MAX_NUM_USERS     16
+
+// Derived
+#define MAX_NUM_BLOCKS  (VDISK_CAPACITY / BLOCK_SIZE)
+#define MAX_FT_SIZE     (MAX_NUM_BLOCKS / FILE_SIZE)
+#define MAX_USER_BLOCKS (MAX_NUM_BLOCKS / MAX_NUM_USERS)
+#define MAX_USER_FILES  (MAX_NUM_BLOCKS / MAX_NUM_USERS)
 
 
 struct _FileTableEntry {
@@ -21,6 +33,21 @@ struct _FileTableEntry {
 struct _FileTable {
     FileTableEntry entry[MAX_FT_SIZE];
 }; typedef struct _FileTable FileTable;
+
+struct _FileInfo {
+    char name[MAX_FILE_NAME_LEN];
+    int  blocks[FILE_SIZE];
+}; typedef struct _FileInfo FileInfo;
+
+struct _User {
+    char name[MAX_USER_NAME_LEN];
+    FileInfo files[MAX_USER_FILES];
+}; typedef struct _User User;
+
+struct _UsersBlocks {
+    char blocks[MAX_NUM_BLOCKS];
+    User users[MAX_NUM_USERS];
+}; typedef struct _UsersBlocks UsersBlocks;
 
 
 FileTable* _initialize_file_table();
