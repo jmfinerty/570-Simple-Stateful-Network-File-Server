a33e0e82fce0183888debdee8e529d0cb98ac4bb
jmfinerty <jmfinerty@gmail.com>
2021-11-16 21:47:41 -0500

    define disk related constants in diskutil

---
+++ diskutil.h
@@ -0,0 +1,26 @@
+#ifndef DISKUTIL_H
+#define DISKUTIL_H
+
+#define DEFAULT_FILE_NAME "SOMEFILE"
+#define DEFAULT_USER_NAME "SOMEUSER"
+
+#define VDISK_LOC "vdisk"
+
+#define MAX_FT_SIZE       200
+#define MAX_FILE_NAME_LEN 20
+#define MAX_USER_NAME_LEN 15
+
+
+struct _FileTableEntry {
+    char ownerUserName[MAX_USER_NAME_LEN];
+    char fileName[MAX_FILE_NAME_LEN];
+    int  fileDescriptor;
+    int  filePointerPos;
+}; typedef struct _FileTableEntry FileTableEntry;
+
+struct _FileTable {
+    FileTableEntry entry[MAX_FT_SIZE];
+}; typedef struct _FileTable FileTable;
+
+
+#endif /* DISKUTIL_H */
--- server.h
+++ server.h
@@ -2,21 +2,3 @@
 #define BLOCK_SIZE     512      // bytes per block
 #define VDISK_CAPACITY 16777216 // 16 mB
 #define MAX_NUM_BLOCKS 32768    // max capacity / size of block
-
-#define VDISK_LOC "vdisk"
-
-#define MAX_FT_SIZE       200
-#define MAX_FILE_NAME_LEN 20
-#define MAX_USER_NAME_LEN 15
-
-
-struct _FileTableEntry {
-    char ownerUserName[MAX_USER_NAME_LEN];
-    char fileName[MAX_FILE_NAME_LEN];
-    int  fileDescriptor;
-    int  filePointerPos;
-}; typedef struct _FileTableEntry FileTableEntry;
-
-struct _FileTable {
-    FileTableEntry table[MAX_FT_SIZE];
-}; typedef struct _FileTable FileTable;
